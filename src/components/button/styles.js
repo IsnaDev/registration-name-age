45c49d03e6b312e2b20d85d1261d4cf3a26491ac
Isnaíra Souza <ariansi_anjo@hotmail.com>
2022-06-30 10:27:50 -0300

    Updated README

--- src/components/button/styles.js
+++ src/components/button/styles.js
@@ -1,4 +1,4 @@
-import styled from 'styled-compontents';
+import styled from 'styled-components';
 
 export const Button = styled.button`
     width: 342px;
--- src/components/title/index.js
+++ src/components/title/index.js
@@ -2,9 +2,9 @@ import React from 'react';
 
 import { H1 } from './styles';
 
-function Title(children) {
+function Title({children}) {
 
-    return <H1>children</H1>
+    return <H1>{children}</H1>
 }
 
 export default Title
